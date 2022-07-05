66ca0cda6268defdbeebacc225d87f3c627a1b7c
John Abramo <jabr@odoo.com>
2022-07-05 10:43:56 -0400

    [ADD] manifest / init

---
+++ recreation/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Recreation',
+    'summary': 'Manage office recreation activities',
+    'description': 'Manage office recreation activities',
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'human_resources',
+    'license': 'OPL-1',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': []
+}
