7f8eb37387177eadfc2f1447e6fba502fe50e596
Orrin Zhong <orzh@odoo.com>
2022-07-05 12:55:05 -0400

    Added Location model

--- recreation/models/recreation_location.py
+++ recreation/models/recreation_location.py
@@ -11,4 +11,3 @@ class RecreationLocation(models.Model):
     name = fields.Char(string='Location Name')
     activity_ids = fields.Many2many(comodel_name='recreation.activity', string='Activities')
     match_ids = fields.One2many(comodel_name='recreation.match', inverse_name='location_id', string='Matches')
-
