c8c63e2717dd77af6e3f094017d39dea64c4f64b
John Abramo <jabr@odoo.com>
2022-07-05 13:52:14 -0400

    [ADD] activity, team, location views

--- recreation/__manifest__.py
+++ recreation/__manifest__.py
@@ -12,6 +12,9 @@
     'depends': ['base'],
     'data': [
         'views/recreation_menuitems.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/recreation_location_views.xml',
+        'views/recreation_activity_views.xml',
+        'views/recreation_team_views.xml'
     ]
 }
