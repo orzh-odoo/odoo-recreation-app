bea44263957a978f716cf6b304858da1420630ae
Orrin Zhong <orzh@odoo.com>
2022-07-05 11:14:40 -0400

    Made matches field of Activity model a one2many field

--- recreation/models/recreation_activity.py
+++ recreation/models/recreation_activity.py
@@ -12,7 +12,7 @@ class RecreationActivity(models.Model):
     description = fields.Char(string='Activity Description')
     rules = fields.Char(string='Rules')
     teams = fields.Many2many(comodel_name='recreation.team', string='Teams')
-    games = fields.Many2many(comodel_name='recreation.game', string='Games')
+    matches = fields.One2many(comodel_name='recreation.match', inverse_name='activity_id',string='Matches')
     locations = fields.Many2many(comodel_name='recreation.location', string='Locations')
     average_game_time = fields.Integer(string='Average Game Time in Minutes')
 
